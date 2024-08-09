afacff063843d58fab41d00f4f2bcdab713c8d57
Michael S.O.S Johnson <113233530+SOSJ001@users.noreply.github.com>
2024-08-09 12:33:43 +0000

    updated

--- src/routes/Dashboard/Favourite_Jobs/+page.svelte
+++ src/routes/Dashboard/Favourite_Jobs/+page.svelte
@@ -6,20 +6,11 @@
 
 <div class="w-full space-y-3">
 	<div class=" flex justify-start font-semibold">
-		<span>Favourite Jobs <span class="text-gray-400">(589)</span></span>
+		<span>Favourite Jobs <span class="text-gray-400">(17)</span></span>
 	</div>
-	<!-- table data below in a grid format -->
-	<div class="grid grid-cols-2 bg-gray-100 p-5 text-center font-mono font-semibold">
-		<!-- table header  -->
-		<div class="">Job</div>
-		<div class="grid grid-cols-3">
-			<div>Date Applied</div>
-			<div>Status</div>
-			<div>Action</div>
-		</div>
-	</div>
-	<!-- table body  -->
-	<div class="grid grid-cols-2 items-center justify-between text-center font-mono">
+
+    <!-- jobs below -->
+	<div class=" flex flex-row justify-between shadow-lg p-2">
 		<div class="flex flex-row gap-2">
 			<div>
 				<div class="h-14 w-14">
@@ -36,20 +27,44 @@
 					</span>
 				</div>
 				<div class="flex w-full flex-row justify-between text-gray-500">
-					<span>Brazil</span>
+					<span>Sierra Leone</span>
 					<span>$50k-80k/month</span>
 				</div>
 			</div>
 		</div>
-		<div class="grid grid-cols-3 items-center justify-center text-sm">
-			<div>Feb 8, 2019 19:28</div>
-			<div class="text-green-700">Active</div>
-			<div>
-				<ActionButton textColor="blue-700" hoverColor="gray-200" buttonBg="gray-100">
-					<span slot="text">View Details</span>
-				</ActionButton>
-			</div>
+		<div>
+			<ActionButton width="[200px]" buttonBg="gray-100" textColor="gray-400">
+				<span slot="text">Deadline Expired</span>
+			</ActionButton>
 		</div>
 	</div>
-	<!-- Table Body ends -->
+
+    <div class=" flex flex-row justify-between shadow-lg p-2">
+		<div class="flex flex-row gap-2">
+			<div>
+				<div class="h-14 w-14">
+					<slot name="companylogo"><img alt="company_logo" class="w-14" src={orange} /></slot>
+				</div>
+			</div>
+			<div class="grid-col-2 flex h-full flex-col items-center justify-center gap-3">
+				<div class="text-md flex items-center justify-center">
+					<span>Techical Support Specialist</span>
+					<span class="text-nowrap rounded-sm bg-green-100 px-2 py-1">
+						<div class="text-xs font-semibold uppercase leading-3 text-green-600">
+							<slot name="role">Remote</slot>
+						</div>
+					</span>
+				</div>
+				<div class="flex w-full flex-row justify-between text-gray-500">
+					<span>Sierra Leone</span>
+					<span>$50k-80k/month</span>
+				</div>
+			</div>
+		</div>
+		<div>
+			<ActionButton hoverColor="blue-200" width="[200px]" buttonBg="blue-100" textColor="blue-700">
+				<span class="font-bold" slot="text">Apply Now</span>
+			</ActionButton>
+		</div>
+	</div>
 </div>
--- src/routes/Dashboard/Job_Alert/+page.svelte
+++ src/routes/Dashboard/Job_Alert/+page.svelte
@@ -1,1 +1,67 @@
-Job_Alert
+<script lang="ts">
+	import Features from '$lib/components/Features.svelte';
+	import ActionButton from '$lib/components/ActionButton.svelte';
+	import orange from '$lib/icon/orange.png';
+</script>
+
+<div class="w-full space-y-3">
+	<div class=" flex justify-start font-semibold">
+		<span>Jobs Alerts <span class="text-gray-400">(17)</span></span>
+	</div>
+	<div class=" font-mono flex flex-row justify-between shadow-lg p-2">
+		<div class="flex flex-row gap-2">
+			<div>
+				<div class="h-14 w-14">
+					<slot name="companylogo"><img alt="company_logo" class="w-14" src={orange} /></slot>
+				</div>
+			</div>
+			<div class="grid-col-2 flex h-full flex-col items-center justify-center gap-3">
+				<div class="text-md flex items-center justify-center">
+					<span>Techical Support Specialist</span>
+					<span class="text-nowrap rounded-sm bg-green-100 px-2 py-1">
+						<div class="text-xs font-semibold uppercase leading-3 text-green-600">
+							<slot name="role">Remote</slot>
+						</div>
+					</span>
+				</div>
+				<div class="flex w-full flex-row justify-between text-gray-500">
+					<span>Sierra Leone</span>
+					<span>$50k-80k/month</span>
+				</div>
+			</div>
+		</div>
+		<div>
+			<ActionButton hoverColor="blue-200" width="[200px]" buttonBg="blue-100" textColor="blue-700">
+				<span class="font-bold" slot="text">Apply Now</span>
+			</ActionButton>
+		</div>
+	</div>
+    <div class="font-mono flex flex-row justify-between shadow-lg p-2">
+		<div class="flex flex-row gap-2">
+			<div>
+				<div class="h-14 w-14">
+					<slot name="companylogo"><img alt="company_logo" class="w-14" src={orange} /></slot>
+				</div>
+			</div>
+			<div class="grid-col-2 flex h-full flex-col items-center justify-center gap-3">
+				<div class="text-md flex items-center justify-center">
+					<span>Techical Support Specialist</span>
+					<span class="text-nowrap rounded-sm bg-green-100 px-2 py-1">
+						<div class="text-xs font-semibold uppercase leading-3 text-green-600">
+							<slot name="role">Remote</slot>
+						</div>
+					</span>
+				</div>
+				<div class="flex w-full flex-row justify-between text-gray-500">
+					<span>Sierra Leone</span>
+					<span>$50k-80k/month</span>
+				</div>
+			</div>
+		</div>
+		<div>
+			<ActionButton hoverColor="blue-200" width="[200px]" buttonBg="blue-100" textColor="blue-700">
+				<span class="font-bold" slot="text">Apply Now</span>
+			</ActionButton>
+		</div>
+	</div>
+</div>
