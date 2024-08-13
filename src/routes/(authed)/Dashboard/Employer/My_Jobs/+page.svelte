e2d630ea61704df7330fdd205033d2970ab213d2
Michael S.O.S Johnson <113233530+SOSJ001@users.noreply.github.com>
2024-08-13 12:11:17 +0000

    Update +page.svelte

--- src/routes/(authed)/Dashboard/Employer/My_Jobs/+page.svelte
+++ src/routes/(authed)/Dashboard/Employer/My_Jobs/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import Features from '$lib/components/Features.svelte';
 	import ActionButton from '$lib/components/ActionButton.svelte';
-    import { Spinner } from 'flowbite-svelte'
-    export let data
+	import { Spinner } from 'flowbite-svelte';
+	export let data;
 	// data.jobTableResult.then((data)=>{console.log("data", data)})
-    let rows = data.jobTableResult;
-    let applicants = 15
+	let rows = data.jobTableResult;
+	let applicants :number;
 </script>
 
 <div class="w-full space-y-3 overflow-y-auto">
@@ -18,43 +18,42 @@
 		<div class="">Jobs</div>
 		<div class="grid grid-cols-3">
 			<div>Applications</div>
-            <div>Status</div>
+			<div>Status</div>
 			<div>Action</div>
 		</div>
 	</div>
 	<!-- table body  -->
-     {#await rows}
-        <Spinner color="blue" size={8} />
-     {:then row} 
-        <div class="p-2 shadow grid grid-cols-2 items-center justify-between text-center font-mono">
-		<div class="flex flex-row gap-2">
-			<div class="grid-col-2 flex h-full flex-col items-center justify-center gap-3 w-full">
-				<div class="text-md flex items-center justify-between bg-red-500 w-full">
-					<span>{row[0].jobTitle}</span>
-					<span class="text-nowrap rounded-sm bg-green-100 px-2 py-1">
-						<div class="text-xs font-semibold uppercase leading-3 text-green-600">
-							<slot name="role">{row[0].remote === null?"":row[0].remote}</slot>
-						</div>
-					</span>
-				</div>
-				<div class="flex w-full flex-row justify-between text-gray-500">
-					<span>Brazil</span>
-					<span>${row[0].minSalary}-{row[0].maxSalary}k/month</span>
+	{#await rows}
+		<Spinner color="blue" size={8} />
+	{:then row}
+		<div class="grid grid-cols-2 gap-5 items-center justify-between p-2 text-center font-mono shadow">
+			<div class="gap- flex flex-row">
+				<div class="grid-col-2 flex h-full w-full flex-col items-center justify-center gap-3">
+					<div class="text-md flex w-full items-center justify-between">
+						<span>{row[0].jobTitle}</span>
+						<span class="text-nowrap rounded-sm bg-green-100 px-2 py-1">
+							<div class="text-xs font-semibold uppercase leading-3 text-green-600">
+								<slot name="role">{row[0].remote === null ? '' : row[0].remote}</slot>
+							</div>
+						</span>
+					</div>
+					<div class="flex w-full flex-row justify-between text-gray-500">
+						<span>{row[0].country}</span>
+						<span>${row[0].minSalary}-{row[0].maxSalary}k/month</span>
+					</div>
 				</div>
 			</div>
+			<div class="grid grid-cols-3 items-center justify-center text-sm text-gray-500">
+				<div>{applicants} Applications</div>
+				<div class="text-green-700">Active</div>
+				<button on:click={() => alert('functionality not active yet')}>
+					<ActionButton textColor="blue-700" hoverColor="gray-200" buttonBg="gray-100">
+						<span slot="text">View Details</span>
+					</ActionButton>
+				</button>
+			</div>
 		</div>
-		<div class="grid grid-cols-3 items-center justify-center text-sm text-gray-500">
-			<div>{applicants} Applications</div>
-			<div class="text-green-700">Active</div>
-			<button on:click={()=> alert("functionality not active yet")}>
-				<ActionButton textColor="blue-700" hoverColor="gray-200" buttonBg="gray-100">
-					<span slot="text">View Details</span>
-				</ActionButton>
-			</button>
-		</div>
-	</div>
-     {/await}
-	
-    
+	{/await}
+
 	<!-- Table Body ends -->
 </div>
