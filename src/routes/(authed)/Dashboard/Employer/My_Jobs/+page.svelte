2d20e86187d4051fa85474e21f7c1f36900cdcc7
Michael S.O.S Johnson <113233530+SOSJ001@users.noreply.github.com>
2024-08-13 12:41:44 +0000

    Update +page.svelte

--- src/routes/(authed)/Dashboard/Employer/My_Jobs/+page.svelte
+++ src/routes/(authed)/Dashboard/Employer/My_Jobs/+page.svelte
@@ -5,7 +5,7 @@
 	export let data;
 	// data.jobTableResult.then((data)=>{console.log("data", data)})
 	let rows = data.jobTableResult;
-	let applicants :number= 0;
+	let applicants: number = 0;
 </script>
 
 <div class="w-full space-y-3 overflow-y-auto">
@@ -26,33 +26,39 @@
 	{#await rows}
 		<Spinner color="blue" size={8} />
 	{:then row}
-		<div class="grid grid-cols-2 gap-5 items-center justify-between p-2 text-center font-mono shadow">
-			<div class="gap- flex flex-row">
-				<div class="grid-col-2 flex h-full w-full flex-col items-center justify-center gap-3">
-					<div class="text-md flex w-full items-center justify-between">
-						<span>{row[0].jobTitle}</span>
-						<span class="text-nowrap rounded-sm bg-green-100 px-2 py-1">
-							<div class="text-xs font-semibold uppercase leading-3 text-green-600">
-								<slot name="role">{row[0].remote === null ? '' : row[0].remote}</slot>
+		{#if row !== null}
+			{#each row as rowdata}
+				<div
+					class="grid grid-cols-2 items-center justify-between gap-5 p-2 text-center font-mono shadow"
+				>
+					<div class="gap- flex flex-row">
+						<div class="grid-col-2 flex h-full w-full flex-col items-center justify-center gap-3">
+							<div class="text-md flex w-full items-center justify-between">
+								<span>{rowdata.jobTitle}</span>
+								<span class="text-nowrap rounded-sm bg-green-100 px-2 py-1">
+									<div class="text-xs font-semibold uppercase leading-3 text-green-600">
+										<slot name="role">{rowdata.remote === null ? '' : rowdata.remote}</slot>
+									</div>
+								</span>
 							</div>
-						</span>
+							<div class="flex w-full flex-row justify-between text-gray-500">
+								<span>{rowdata.country}</span>
+								<span>${rowdata.minSalary}-{rowdata.maxSalary}k/month</span>
+							</div>
+						</div>
 					</div>
-					<div class="flex w-full flex-row justify-between text-gray-500">
-						<span>{row[0].country}</span>
-						<span>${row[0].minSalary}-{row[0].maxSalary}k/month</span>
+					<div class="grid grid-cols-3 items-center justify-center text-sm text-gray-500">
+						<div>{applicants} Applications</div>
+						<div class="text-green-700">Active</div>
+						<button on:click={() => alert('functionality not active yet')}>
+							<ActionButton textColor="blue-700" hoverColor="gray-200" buttonBg="gray-100">
+								<span slot="text">View Details</span>
+							</ActionButton>
+						</button>
 					</div>
 				</div>
-			</div>
-			<div class="grid grid-cols-3 items-center justify-center text-sm text-gray-500">
-				<div>{applicants} Applications</div>
-				<div class="text-green-700">Active</div>
-				<button on:click={() => alert('functionality not active yet')}>
-					<ActionButton textColor="blue-700" hoverColor="gray-200" buttonBg="gray-100">
-						<span slot="text">View Details</span>
-					</ActionButton>
-				</button>
-			</div>
-		</div>
+			{/each}
+		{/if}
 	{/await}
 
 	<!-- Table Body ends -->
